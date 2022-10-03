941cc1ee52e143067eab6368f024684efa9fcedd
Rizki Khair <44086375+rizuki4658@users.noreply.github.com>
2022-10-03 21:01:03 +0700

    feat: final intialize container of corousel

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -4,9 +4,11 @@ const Carousel: React.FC<{}> = ({...props}) => {
   return (
     <div className="bg-red-500 container-carousel">
       <div className="relative h-full w-full">
-        <button className="bg-pink-500 absolute left-0 top-0 bottom-0 w-24">PREV</button>
-        <a href="#" className="bg-blue-500 w-full h-full block">AAAA</a>
-        <button className="bg-pink-500 absolute right-0 top-0 bottom-0 w-24">NEXT</button>
+        <button className="absolute left-0 top-0 bottom-0 w-24" />
+        <div className="bg-blue-500 w-full h-full block">
+          <a href="#" className="bg-blue-500 w-full h-full block">AAAA</a>
+        </div>
+        <button className="absolute right-0 top-0 bottom-0 w-24" />
       </div>
     </div>
   )
