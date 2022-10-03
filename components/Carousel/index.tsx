b010df9b7ef13d8f9e5574ca8082cdf7f75b8c47
Rizki Khair <44086375+rizuki4658@users.noreply.github.com>
2022-10-03 20:35:02 +0700

    feat: file carousel component

---
+++ components/Carousel/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Carousel: React.FC<{}> = ({...props}) => {
+  return (
+    <div>Carousel</div>
+  )
+}
+
+export default Carousel
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -40,11 +40,13 @@ class Header extends React.Component<StateHeader, StateHeader> {
 
   render() {
     return (
-      <header className="flex items-center md:py-3 md:px-8 py-2 px-4 gap-x-6">
-        <Slider mobile={this.state.mobile} />
-        <Logo />
-        <Navigation mobile={this.state.mobile}/>
-        <SearchLogin mobile={this.state.mobile} />
+      <header className="fixed top-0 left-0 right-0">
+        <div className="flex items-center md:py-3 md:px-8 py-2 px-4 gap-x-6">
+          <Slider mobile={this.state.mobile} />
+          <Logo />
+          <Navigation mobile={this.state.mobile}/>
+          <SearchLogin mobile={this.state.mobile} />
+        </div>
       </header>
     )
   }
