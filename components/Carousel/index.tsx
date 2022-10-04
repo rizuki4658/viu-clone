0d55ab4f19fd2b29d083e8f9499a9befd19621af
Rizki Khair <44086375+rizuki4658@users.noreply.github.com>
2022-10-04 18:15:45 +0700

    feat: constant for carousel

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 
-const Carousel: React.FC<{}> = ({...props}) => {
-  return (
-    <div className="bg-red-500 container-carousel">
-      <div className="relative h-full w-full">
-        <button className="absolute left-0 top-0 bottom-0 w-24" />
-        <div className="bg-blue-500 w-full h-full block">
-          <a href="#" className="bg-blue-500 w-full h-full block">AAAA</a>
+const autoplay = setInterval(() => {
+
+}, 4000)
+
+class Carousel extends React.Component {
+  render() {
+    return (
+      <div className="container-carousel">
+        <div className="relative h-full w-full">
+          <button className="absolute left-0 top-0 bottom-0 w-24" />
+          <div className="w-full h-full block">
+            <a href="#" className="bg-blue-500 w-full h-full block">AAAA</a>
+          </div>
+          <button className="absolute right-0 top-0 bottom-0 w-24" />
         </div>
-        <button className="absolute right-0 top-0 bottom-0 w-24" />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Carousel
---
+++ constants/carousel.ts
@@ -0,0 +1,22 @@
+export const sliders = [
+  {
+    name: '',
+    link: '',
+    img: 'slider-one.webp'
+  },
+  {
+    name: '',
+    link: '',
+    img: 'slider-two.webp'
+  },
+  {
+    name: '',
+    link: '',
+    img: 'slider-three.webp'
+  },
+  {
+    name: '',
+    link: '',
+    img: 'slider-four.webp'
+  }
+]
