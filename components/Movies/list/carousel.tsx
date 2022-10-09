84c58d6c9018f6ec220167662cd280fd2a208bc3
Rizki Khair <44086375+rizuki4658@users.noreply.github.com>
2022-10-09 18:06:25 +0700

    feat: prototype detail info

--- components/Movies/list/carousel.tsx
+++ components/Movies/list/carousel.tsx
@@ -1,4 +1,5 @@
 import React,{ createRef } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import {
   MoviePropsType,
   MovieStateType
@@ -17,14 +18,24 @@ class CarouselLists extends React.Component<MoviePropsType, MovieStateType> {
       refChild: createRef(),
       refParent: createRef(),
       showLink: false,
-      refCard: createRef()
+      refCard: createRef(),
+      classDetail: [
+        'absolute top-1/2',
+        'transform -translate-y-1/2',
+        'bg-pink-400 w-72 h-80 rounded-3xl',
+        'transition-all duration-300 ease-out',
+        'scale-0 invisible opacity-0 z-0'
+      ]
     }
 
     this.nextClick = this.nextClick.bind(this)
     this.prevClick = this.prevClick.bind(this)
     this.showingButtonControl = this.showingButtonControl.bind(this)
     this.showingDetail = this.showingDetail.bind(this)
+    this.hidingDetail = this.hidingDetail.bind(this)
   }
+  showTime: any
+  hideTime: any
 
   stateSetter(key: string, value: any) {
     this.setState((oldState) => ({
@@ -55,8 +66,30 @@ class CarouselLists extends React.Component<MoviePropsType, MovieStateType> {
     this.stateSetter('showButton', child.scrollWidth > parent.offsetWidth)
   }
 
-  showingDetail() {
-    console.log(this.state.refCard)
+  showingDetail(e: any) {
+    clearTimeout(this.hideTime)
+    
+    const target: HTMLElement = e?.target
+    const rect = target?.getBoundingClientRect()
+    const cardInfo = this.state.refCard.current
+    console.log(rect)
+    cardInfo.style.left = rect.x > 35  ? `${rect.x - 84}px` : '16px'
+    const oldClass = [...this.state.classDetail]
+    if (oldClass[oldClass.length - 1] === 'scale-1 visible opacity-100 z-20') return
+    oldClass[oldClass.length - 1] = 'scale-1 visible opacity-100 z-20'
+    this.showTime = setTimeout(() => {
+      this.stateSetter('classDetail', oldClass)
+    }, 300)
+  }
+
+  hidingDetail() {
+    clearTimeout(this.showTime)
+    const oldClass = [...this.state.classDetail]
+    if (oldClass[oldClass.length - 1] === 'scale-0 invisible opacity-0 z-0') return
+    oldClass[oldClass.length - 1] = 'scale-0 invisible opacity-0'
+    this.hideTime = setTimeout(() => {
+      this.stateSetter('classDetail', oldClass)
+    }, 300)
   }
 
   componentDidMount(): void {
@@ -70,10 +103,12 @@ class CarouselLists extends React.Component<MoviePropsType, MovieStateType> {
           {...this.props.title}
           showLink={this.state.showLink}
         />
-        <div className="w-full relative bg-pink-500" style={{ minHeight: '150px' }}>
+        <div
+          className="w-full relative mb-10"
+          style={{ minHeight: '150px' }}>
           <div
             ref={this.state.refParent}
-            className="relative block">
+            className="z-10 relative block">
             <ButtonControl
               show={this.state.showButton}
               type="prev"
@@ -83,7 +118,50 @@ class CarouselLists extends React.Component<MoviePropsType, MovieStateType> {
               ref={this.state.refChild}
               className="carousel-movie-list md:h-60 h-32 block whitespace-nowrap space-x-4 overflow-hidden">
               <div 
-                ref={this.state.refCard}
+                className="inline-block h-full md:w-40 w-20"
+                onMouseEnter={this.showingDetail}>
+                <CardMovie
+                  {...{
+                    img: '',
+                    name: '1',
+                    desc: ''
+                  }}
+                />
+              </div>
+              <div 
+                className="inline-block h-full md:w-40 w-20"
+                onMouseEnter={this.showingDetail}>
+                <CardMovie
+                  {...{
+                    img: '',
+                    name: '1',
+                    desc: ''
+                  }}
+                />
+              </div>
+              <div 
+                className="inline-block h-full md:w-40 w-20"
+                onMouseEnter={this.showingDetail}>
+                <CardMovie
+                  {...{
+                    img: '',
+                    name: '1',
+                    desc: ''
+                  }}
+                />
+              </div>
+              <div 
+                className="inline-block h-full md:w-40 w-20"
+                onMouseEnter={this.showingDetail}>
+                <CardMovie
+                  {...{
+                    img: '',
+                    name: '1',
+                    desc: ''
+                  }}
+                />
+              </div>
+              <div 
                 className="inline-block h-full md:w-40 w-20"
                 onMouseEnter={this.showingDetail}>
                 <CardMovie
@@ -100,6 +178,13 @@ class CarouselLists extends React.Component<MoviePropsType, MovieStateType> {
               type="next"
               callback={this.nextClick}
             />
+          </div>
+
+          <div
+            ref={this.state.refCard}
+            className={this.state.classDetail.join(' ')}
+            onMouseOut={this.hidingDetail}>
+            AA
           </div>
         </div>
       </>
