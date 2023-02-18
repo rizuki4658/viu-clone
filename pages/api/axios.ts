d4b63f7bd9c1f12aaa3d1240f46b4b7297e35ec2
rizuki4658 <rizkikhair4658@gmail.com>
2023-02-19 00:45:35 +0700

    fix: api server

--- pages/api/axios.ts
+++ pages/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Axios = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/rizuki4658/viu-clone-api/'
+  baseURL: 'https://api.npoint.io/d5a23ee80d3a5a0822a4/'
 })
 
 export default Axios
